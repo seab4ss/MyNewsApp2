442f9e5d7a9df5ca186abd1b523abc09ab3ffa62
yurrr <0verstayw@gmail.com>
2020-08-31 16:58:29 -0500

    added error message

--- app/src/main/java/com/example/mynewsapp/Utils.kt
+++ app/src/main/java/com/example/mynewsapp/Utils.kt
@@ -3,6 +3,7 @@ package com.example.mynewsapp
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 
 
 /**
@@ -11,6 +12,7 @@ import android.net.Uri
 
 class Utils {
     companion object {
+        const val TAG = "NewsUtils"
         fun openWebPage(context: Context, url: String) {
             val webPage: Uri = Uri.parse(url)
             val intent = Intent(Intent.ACTION_VIEW, webPage)
@@ -22,6 +24,8 @@ class Utils {
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             if (intent.resolveActivity(context.packageManager) != null) {
                 context.startActivity(intent)
+            } else {
+                Log.e(TAG, "openWebPage unable to resolve activity to handle uri $webPage")
             }
         }
     }
